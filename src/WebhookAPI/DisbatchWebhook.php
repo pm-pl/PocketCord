0db60f2849f47522d5ab07b826a103c4182d93d8
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-05-04 18:19:03 -0500

    redone!

---
+++ src/WebhookAPI/DisbatchWebhook.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CJMustard1452\PocketCord\WebhookAPI;
+use CJMustard1452\PocketCord\Lib\DataModule;
+
+class DisbatchWebhook {
+
+    private $dataModule;
+
+    final function __construct(DataModule $dataModule) {
+        $this->dataModule = $dataModule;
+        if(!$dataModule->checkWebhookURL()) return;
+        if(!$dataModule->getCache()) return;
+        $this->sendWebhook();
+        $this->dataModule->clearCache();
+    }
+
+    private function sendWebhook():void {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->dataModule->getWebhookURL());
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json; charset=utf-8']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['content' => $this->dataModule->cleanCache() ]));
+        $response = curl_exec($ch);
+    }
+}
