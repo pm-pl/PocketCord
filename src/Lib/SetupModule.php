4660da10feb958265fedaa883632d800ea2fe18e
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-05-11 22:37:10 -0500

    Update SetupModule.php

--- src/Lib/SetupModule.php
+++ src/Lib/SetupModule.php
@@ -2,10 +2,14 @@
 
 namespace CJMustard1452\PocketCord\Lib;
 
+use CJMustard1452\PocketCord\WebhookAPI\WebhookTask;
+use CJMustard1452\PocketCord\Main;
+
 class SetupModule {
 
-    final function __construct(DataModule $dataModule) {
+    final function __construct(Main $main, DataModule $dataModule) {
 		if($dataModule->checkWebhookURL()) return;
         $dataModule->sendWarning('Please use /pocketcord (Webhook URL) to enable logging.');
+        $main->getScheduler()->scheduleRepeatingTask(new WebhookTask($dataModule), 10);
     } 
 }
