e2364a54d350c66d450b1e921b27ae7b560b7ce7
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-05-11 22:36:55 -0500

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -5,7 +5,6 @@ namespace CJMustard1452\PocketCord;
 use CJMustard1452\PocketCord\Lib\DataModule;
 use CJMustard1452\PocketCord\Commands\PocketcordCommand;
 use CJMustard1452\PocketCord\Lib\SetupModule;
-use CJMustard1452\PocketCord\WebhookAPI\WebhookTask;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
@@ -13,8 +12,7 @@ class Main extends PluginBase {
 	protected function onEnable(): void {
 		$dataModule = new DataModule($this);
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($dataModule), $this);
-		$this->getScheduler()->scheduleRepeatingTask(new WebhookTask($dataModule), 10);
-                $this->getServer()->getCommandMap()->register("pocketcord", new PocketcordCommand($dataModule));
-		new SetupModule($dataModule);
+        	$this->getServer()->getCommandMap()->register("pocketcord", new PocketcordCommand($dataModule));
+		new SetupModule($this, $dataModule);
 	}
 }
